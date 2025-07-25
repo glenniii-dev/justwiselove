00fb3680433f0aad8b08171e69e6e99d8bd1b8ad
Glenn <glenniii.dev@gmail.com>
2025-07-25 17:40:47 -0400

    Moved out of JWL

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header.jsx';
+import Main from './components/Main.jsx';
+
+function App() {
+  return (
+    <div className="bg-zinc-950">
+      <Header />
+      <Main />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/main.jsx
@@ -0,0 +1,10 @@
+export default function Main() {
+  return (
+    <main className="bg-zinc-900 text-lime-600 w-[400px] sm:w-[540px] md:w-[668px] lg:w-[924px] py-5 flex items-center h-500 px-5 rounded-lg text-center flex-col my-12 mx-auto justify-self-center wrap-break-word">
+      <section id="home" className="mb-8">
+        <h2 className="text-lg font-thin">"He has told you, O man, what is good. And what is Jehovah requiring of you? Only to exercise justice, to cherish loyalty, And to walk in modesty with your God!"- Micah 6:8</h2>
+        <p className="mt-2"></p>
+      </section>
+    </main>
+  );
+}
