faf92f5f810df2cfc9e74e683ce649d43784a7c6
Glenn <glenniii.dev@gmail.com>
2025-07-26 16:45:30 -0400

    Edited Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="bg-zinc-900 text-lime-600 w-[400px] sm:w-[540px] md:w-[668px] lg:w-[924px] py-4 flex items-center h-auto rounded-lg text-center flex-col my-12 mx-auto justify-self-center flex-wrap">
       <h1 className="font-['Audiowide'] text-6xl font-bold mx-auto text-shadow-black text-shadow-lg w-5xs border-b-3 border-b-lime-600 pb-2">JWL</h1>
-      <h2 className="font-['Audiowide'] text-sm font-thin mx-auto w-5xs text-shadow-black text-shadow-lg pt-2">Just Wise Love</h2>
+      <h2 className="font-['Audiowide'] text-[17px] font-thin mx-auto w-5xs text-shadow-black text-shadow-lg pt-2">Just Wise Love</h2>
     </header>
   );
 }
