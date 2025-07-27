bad48ce4e08eb28cd8d9a9675a69969d8fec06b2
Glenn <glenniii.dev@gmail.com>
2025-07-26 20:12:49 -0400

    Added .5 to just wise love

---
+++ src/components/FamilyWorship.jsx
@@ -0,0 +1,11 @@
+export default function FamilyWorship() {
+  return (
+      <div className="mt-10 bg-zinc-950 text-lime-600 w-full p-4 rounded-lg shadow-lg">
+        <h2 className="text-5xl font-thin mb-5">Family Worship</h2>
+        <h4 className="text-3xl font-thin mb-4">Week Of: DATE</h4>
+        <div className="bg-zinc-900 p-4 rounded-lg">
+          <p>CONTENT</p>
+        </div>
+      </div>
+    )
+}
