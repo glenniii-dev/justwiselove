c557df89428f831bb1a4c0554fe9c7cf3d1d0562
Glenn <glenniii.dev@gmail.com>
2025-08-01 14:15:16 -0400

    Fixed Logo to new logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
+import jwl from '../../public/jwl.png';
+
 export default function Header() {
   return (
     <header className="bg-zinc-900 text-white w-[400px] sm:w-[540px] md:w-[668px] lg:w-[924px] py-4 flex items-center h-auto rounded-lg text-center flex-col my-12 mx-auto justify-self-center flex-wrap">
-      <h1 className="font-['Audiowide'] text-6xl font-bold mx-auto text-shadow-black text-shadow-lg w-5xs border-b-3 border-b-lime-600 pb-2">JWL</h1>
-      <h2 className="font-['Audiowide'] text-[17.5px] font-thin mx-auto w-5xs text-shadow-black text-shadow-lg pt-2">Just Wise Love</h2>
+      <img src={jwl} alt="Jwl" className="w-25" />
+      {/* <h1 className="font-['Audiowide'] text-6xl font-bold mx-auto text-shadow-black text-shadow-lg w-5xs border-b-3 border-b-lime-600 pb-2">JWL</h1>*/}
+      <h2 className="font-['Audiowide'] text-[17.5px] font-thin mx-auto w-5xs text-shadow-black text-shadow-lg pt-2 border-t-3 border-t-lime-600">Just Wise Love</h2>
     </header>
   );
 }
