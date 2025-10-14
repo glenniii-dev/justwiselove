64695e43ba2b07ae660a4f727d27e5c450f87e73
Glenn Hensley III <glenniii.dev@gmail.com>
2025-10-14 10:02:13 -0400

    Connect front end to backend

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,9 +8,9 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://justwiseloveserver.vercel.app",
         changeOrigin: true,
-        secure: false,
+        secure: true,
       },
     },
   },
