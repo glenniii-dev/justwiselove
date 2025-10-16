f290ca668a91e5b61eca6983bf210150df9f43f0
Glenn Hensley III <glenniii.dev@gmail.com>
2025-10-16 15:42:47 -0400

    adjustments for deploying

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components/header/header';
 import { Footer } from './shared/components/footer/footer';
 import { Home } from "./features/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, Home],
+  imports: [Header, Footer, Home],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
